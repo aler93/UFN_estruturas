9de340c5c7b622ab057269773dc7c68163b1e737
Alisson Naimayer <anaimayer3@gmail.com>
2020-03-31 13:40:55 -0300

    Tab ident

--- 1_revisaoAlgo2/arquivo1.c
+++ 1_revisaoAlgo2/arquivo1.c
@@ -2,31 +2,31 @@
 #include <stdlib.h>
 
 int main() {
-    //programa que le um nome de arquivo e se existe exibe seu conteudo
-    //na tela. OBS.: arquivos text
-    FILE *procurador;
-    char nomeArquivo[200];
+	//programa que le um nome de arquivo e se existe exibe seu conteudo
+	//na tela. OBS.: arquivos text
+	FILE *procurador;
+	char nomeArquivo[200];
 
-    printf("Digite o nome do arquivo: ");
-    scanf("%s", nomeArquivo);
+	printf("Digite o nome do arquivo: ");
+	scanf("%s", nomeArquivo);
 
-    procurador = fopen(nomeArquivo, "r");
+	procurador = fopen(nomeArquivo, "r");
 
-    if (!procurador) {
-        printf("Arquivo nao encontrado!\n");
-        exit(0);
-    }
-    printf("Arquivo localizado e sendo aberto...\n");
+	if (!procurador) {
+		printf("Arquivo nao encontrado!\n");
+		exit(0);
+	}
+	printf("Arquivo localizado e sendo aberto...\n");
 
-    char linha[200];
-    int contaLinhas = 0;
-    do {
-        contaLinhas++;
-        fgets(linha, 200, procurador);
-        printf("%s", linha);
-    } while (!feof(procurador));
+	char linha[200];
+	int contaLinhas = 0;
+	do {
+		contaLinhas++;
+		fgets(linha, 200, procurador);
+		printf("%s", linha);
+	} while (!feof(procurador));
 
-    printf("Este arquivo tem %d linhas\n", contaLinhas);
-    fclose(procurador);
-    return 1;
+	printf("Este arquivo tem %d linhas\n", contaLinhas);
+	fclose(procurador);
+	return 1;
 }
