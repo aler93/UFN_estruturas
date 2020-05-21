bee86c7e218ef8130defc376ce6938c8bf4ac19a
Alisson Naimayer <anaimayer3@gmail.com>
2020-05-21 18:57:14 -0300

    Arvores

--- 8_arvores/arvore1.c
+++ 8_arvores/arvore1.c
@@ -3,7 +3,7 @@
 #include <time.h>
 #include <locale.h>
 
-#define TAM 5
+#define TAM 10
 
 typedef struct no { //arvore binaria
 	int conteudo;
@@ -24,7 +24,7 @@ Arvore *inserir(int valor, Arvore *r) { //Arvore Binaria de Pesquisa ou Ordenada
 		return novo;
 	}
 }
-
+//codigo do livro C Completo e Total de Robert Schild
 void exibir(Arvore *r, int nivel) {
 	if (r) {
 		exibir(r->dir, nivel + 1);//vai para direita
@@ -50,10 +50,22 @@ Arvore *localizar(int valor, Arvore *r){
 
 int localizarNivel(int valor, Arvore *r) {
 	if (r) {
-		
+		if (valor == r->conteudo) return 0;
+		if (valor < r->conteudo)  {
+			//vai para esquerda
+			int nivel = localizarNivel(valor, r->esq);
+			if (nivel == -1) return -1;
+			else return nivel + 1;
+		} else {
+			//vai para direita
+			int nivel = localizarNivel(valor, r->dir);
+			if (nivel == -1) return -1;
+			else return nivel + 1;
+		}
 	}
-	return -1;
+	return -1; //se chegou aqui, pq o valor não foi encontrado
 }
+
 
 int contarNos(Arvore *r) {
 	if (r) {
@@ -65,9 +77,9 @@ int contarNos(Arvore *r) {
 int contarNosFolhas(Arvore *r) {
 	if (r) {
 		if (!r->esq && !r->dir) {
-			return 1;
+			return 1; //+ contarNosFolhas(r->esq) + contarNosFolhas(r->dir);
 		}
-		return 0 + contarNosFolhas(r->esq) + contarNosFolhas(r->dir);
+		return  0 + contarNosFolhas(r->esq) + contarNosFolhas(r->dir);
 	}
 	return 0;
 }
@@ -75,16 +87,39 @@ int contarNosFolhas(Arvore *r) {
 int contarNosNaoFolhas(Arvore *r) {
 	if (r) {
 		if (!r->esq && !r->dir) {
-			return 0;
+			return 0; //+ contarNosFolhas(r->esq) + contarNosFolhas(r->dir);
 		}
 		return 1 + contarNosNaoFolhas(r->esq) + contarNosNaoFolhas(r->dir);
 	}
 	return 0;
 }
 
+void mostrarNosFolhas(Arvore *r) {
+	if (r) {
+		mostrarNosFolhas(r->esq);
+
+		if (!r->esq && !r->dir) {
+			printf("%d\t",r->conteudo);
+		} 
+
+		mostrarNosFolhas(r->dir);
+	}
+}
+
 int somarNos(Arvore *r) {
 	if (r) {
 		return r->conteudo+ somarNos(r->esq) + somarNos(r->dir);
+	}
+	return 0;
+}
+
+int calcularAltura(Arvore *r) {
+	if (r) {
+		int alturaEsquerda = calcularAltura(r->esq);
+		int alturaDireita = calcularAltura(r->dir);
+		
+		if (alturaEsquerda > alturaDireita) return 1 + alturaEsquerda;
+		return 1 + alturaDireita;
 	}
 	return 0;
 }
@@ -103,12 +138,15 @@ int main() {
 	
 	printf("Exibe arvore deitada a partir do nivel 0\n");
 	exibir(raiz, 0);
+
+	printf("A altura da árvore é: %d\n", calcularAltura(raiz));
+	printf("Total de nós folhas: %d\n", contarNosFolhas(raiz));
+	mostrarNosFolhas(raiz);
 	
-	printf("Informe um inteiro para pesquisa na arvore: ");
+	printf("\nInforme um inteiro para pesquisa na arvore: ");
 	scanf("%d", &valor);
 	
-	printf("Endereco de %d na arvore: %d\n", valor, localizarNivel(valor, raiz));
-	
+	printf("Nivel de %d na arvore: %d\n", valor, localizarNivel(valor, raiz));
 	
 	return 1;
 }
